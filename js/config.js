390a682be1eb6901a22e9bed1a21c075a48cec8a
LawGlitch-net <lawglitch@gmail.com>
2026-02-26 09:57:18 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const LAWGLITCH_CONFIG = {
   SUPABASE_URL: "https://pxwbumtpnzbbqzqhnsow.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4d2J1bXRwbnpiYnF6cWhuc293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNjM4NTcsImV4cCI6MjA3MzgzOTg1N30.CfDyCEQCCoc4p1KrE_G43ToVGExuVvGAGWk3hMQxf3s",
   EDGE_FUNCTION_URL: "https://pxwbumtpnzbbqzqhnsow.supabase.co/functions/v1/admin-data",
   RATE_LIMIT: {
     MAX_SUBMISSIONS: 3,
